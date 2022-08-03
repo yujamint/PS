f3720d3b9a7ccca47978dc9e8e941fa53da08417
yujaemin <woawoa0513@gmail.com>
2022-08-03 11:47:29 +0900

    move repository

---
+++ BOJ/src/입출력/Add_AB_3_10950/Main.java
@@ -0,0 +1,14 @@
+package 입출력.Add_AB_3_10950;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
---
+++ BOJ/src/입출력/Add_AB_4_10951/Main.java
@@ -0,0 +1,7 @@
+package 입출력.Add_AB_4_10951;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
