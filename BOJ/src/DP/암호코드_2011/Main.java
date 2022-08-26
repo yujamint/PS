6d3ecca020d07c4008f268223725b03c13ca8008
yujaemin <woawoa0513@gmail.com>
2022-08-26 22:55:48 +0900

    20220826

--- BOJ/src/DP/암호코드_2011/Main.java
+++ BOJ/src/DP/암호코드_2011/Main.java
@@ -3,7 +3,6 @@ package DP.암호코드_2011;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,32 +12,37 @@ public class Main {
         String str = br.readLine();
         int n = str.length();
 
-        String[] tokens = str.split("");
-        String[] arr = new String[n + 1];
-        arr[0] = "0";
-        for (int i = 1; i <= n; i++) {
-            arr[i] = tokens[i - 1];
-        }
-
-        int[] dy = new int[n + 1];
+        long[] dy = new long[n + 1];
         dy[0] = 1;
 
         if (n >= 1) dy[1] = 1;
+        if (str.charAt(0) == '0') {
+            System.out.println(0);
+            return;
+        }
 
         for (int i = 2; i <= n; i++) {
-            if (Integer.parseInt(arr[i - 1] + arr[i]) == 0) {
-                System.out.println(0);
-                return;
-            }
-            dy[i] = dy[i - 1];
-            if (i < n) {
-                if (Integer.parseInt(arr[i]) != 0 && arr[i + 1].equals("0")) {
-                    dy[i + 1] = dy[i - 1];
-                    i++;
-                    continue;
+            int one = str.charAt(i-1) - '0';
+            int two = str.charAt(i-2) - '0';
+            if (one != 0) {
+                dy[i] += dy[i-1];
+                dy[i] %= dividor;
+
+                if (two * 10 + one <= 26 && two * 10 + one > 10) {
+                    dy[i] += dy[i-2];
+                    dy[i] %= dividor;
                 }
             }
-            if (Integer.parseInt(arr[i - 1] + arr[i]) <= 26) dy[i] = (dy[i] + dy[i - 2]) % dividor;
+            else { // one == 0일 때,
+                if (two == 1 || two == 2) {
+                    dy[i] += dy[i-2];
+                    dy[i] %= dividor;
+                }
+                else {
+                    System.out.println(0);
+                    return;
+                }
+            }
         }
 
         System.out.println(dy[n] % dividor);
---
+++ BOJ/src/기초수학/최대공약수와최소공배수_2609/Main.java
@@ -0,0 +1,27 @@
+package 기초수학.최대공약수와최소공배수_2609;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int min = Math.min(a, b);
+
+        int gcd = 0;
+        int lcm = 0;
+
+        for (int i = 1; i <= min; i++) {
+            if (a % i == 0 && b % i == 0) gcd = i;
+        }
+
+        lcm = a * b / gcd;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+
+    }
+}
