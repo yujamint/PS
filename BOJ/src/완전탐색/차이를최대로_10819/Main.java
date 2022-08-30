ee4c7280513152a18dcef3faf2f934cb4051a5c8
yujaemin <woawoa0513@gmail.com>
2022-08-30 11:34:00 +0900

    20220830

--- BOJ/src/완전탐색/차이를최대로_10819/Main.java
+++ BOJ/src/완전탐색/차이를최대로_10819/Main.java
@@ -6,14 +6,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int[] arr;
-    public void DFS(int L, int[] cn) {
+    static int n, answer = Integer.MIN_VALUE;
+    static int[] arr, selected, ch;
+    public static void DFS(int L) {
         if (L == n) {
-
+            answer = Math.max(getResult(), answer);
         }
         else {
-
+            for (int i = 0; i < n; i++) {
+                if (ch[i] != 1) {
+                    ch[i] = 1;
+                    selected[L] = arr[i];
+                    DFS(L+1);
+                    ch[i] = 0;
+                }
+            }
         }
     }
 
@@ -22,12 +29,24 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
+        ch = new int[n];
+        selected = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        DFS(0);
+        System.out.println(answer);
 
+    }
+
+    public static int getResult() {
+        int sum = 0;
+        for (int i = 0; i < n-1; i++) {
+            sum += Math.abs(selected[i] - selected[i+1]);
+        }
+        return sum;
     }
 }
