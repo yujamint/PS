e47fb05c8e5e1b37e2ca9899af4817c20c6339f3
yujaemin <woawoa0513@gmail.com>
2022-09-21 23:04:31 +0900

    20220921 BFS로 다시 해결

--- BOJ/src/완전탐색/알고스팟_1261/Main.java
+++ BOJ/src/완전탐색/알고스팟_1261/Main.java
@@ -3,34 +3,65 @@ package 완전탐색.알고스팟_1261;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+class Point implements Comparable<Point> {
+    int x;
+    int y;
+    int count;
+
+    public Point(int x, int y, int count) {
+        this.x = x;
+        this.y = y;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        return this.count - o.count;
+    }
+}
+
 public class Main {
-    static int n, m, answer = Integer.MAX_VALUE;
+    static int n, m;
     static int[][] maze;
-    static boolean[][] visited;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
 
-    public static void DFS(int x, int y, int count) {
-        if (count >= answer) return;
-        if (x == m && y == n) {
-            answer = Math.min(answer, count);
-        } else {
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+    public static int BFS(int x, int y) {
+        PriorityQueue<Point> queue = new PriorityQueue<>();
 
-                if (nx >= 1 && nx <= m && ny >= 1 && ny <= n) {
-                    if (!visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        if (maze[nx][ny] == 1) DFS(nx, ny, count + 1);
-                        else DFS(nx, ny, count);
-                        visited[nx][ny] = false;
-                    }
+        queue.offer(new Point(x, y, 0));
+        boolean[][] visited = new boolean[n + 1][m + 1];
+        visited[x][y] = true;
+
+        int nx;
+        int ny;
+        while (!queue.isEmpty()) {
+            Point cur = queue.poll();
+
+            if (cur.x == n && cur.y == m) return cur.count;
+
+            for (int i = 0; i < 4; i++) {
+                nx = cur.x + dx[i];
+                ny = cur.y + dy[i];
+
+                if (nx < 1 || ny < 1 || nx > n || ny > m) continue;
+
+                if (!visited[nx][ny] && maze[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    queue.offer(new Point(nx, ny, cur.count + 1));
                 }
+
+                if (!visited[nx][ny] && maze[nx][ny] == 0) {
+                    visited[nx][ny] = true;
+                    queue.offer(new Point(nx, ny, cur.count));
+                }
+
             }
         }
+        return -1;
     }
 
     public static void main(String[] args) throws IOException {
@@ -38,21 +69,17 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
-        maze = new int[m + 1][n + 1];
-        visited = new boolean[m + 1][n + 1];
+        maze = new int[n + 1][m + 1];
 
-        for (int i = 1; i <= m; i++) {
+        for (int i = 1; i <= n; i++) {
             String input = br.readLine();
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= m; j++) {
                 maze[i][j] = input.charAt(j - 1) - '0';
             }
         }
-
-        visited[1][1] = true;
-        DFS(1, 1, 0);
-        System.out.println(answer);
+        System.out.println(BFS(1,1));
     }
 }
