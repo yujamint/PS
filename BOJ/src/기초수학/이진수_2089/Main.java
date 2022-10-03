67bfe48cc73d42ef93f3c7fad294716885fa1770
yujaemin <woawoa0513@gmail.com>
2022-10-03 21:46:38 +0900

    20221003

---
+++ BOJ/src/기초수학/이진수_2089/Main.java
@@ -0,0 +1,7 @@
+package 기초수학.이진수_2089;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- BOJ/src/기초수학/팔진수이진수_1212/Main.java
+++ BOJ/src/기초수학/팔진수이진수_1212/Main.java
@@ -1,27 +1,28 @@
+package 기초수학.팔진수이진수_1212;
+
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      
-      String str = sc.next();
-      
-      StringBuilder sb = new StringBuilder("");
-      sb.append(Integer.toBinaryString(str.charAt(0) - '0'));
-      
-      for (int i = 1; i < str.length(); i++){
-          int c = str.charAt(i) - '0';
-          
-          if(c >= 4) sb.append(Integer.toBinaryString(c));
-          
-          else if (c < 4 && c >= 2) {
-              sb.append("0").append(Integer.toBinaryString(c));
-          }
-          
-          else {
-              sb.append("00").append(Integer.toBinaryString(c));
-          }
-      }
-      
-      System.out.println(sb.toString());
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        StringBuilder sb = new StringBuilder("");
+        sb.append(Integer.toBinaryString(str.charAt(0) - '0'));
+
+        for (int i = 1; i < str.length(); i++) {
+            int c = str.charAt(i) - '0';
+
+            if (c >= 4) sb.append(Integer.toBinaryString(c));
+
+            else if (c < 4 && c >= 2) {
+                sb.append("0").append(Integer.toBinaryString(c));
+            } else {
+                sb.append("00").append(Integer.toBinaryString(c));
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
