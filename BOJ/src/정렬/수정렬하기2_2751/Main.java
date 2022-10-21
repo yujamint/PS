57d81d97a291e352c881c9cd12e7d84f2a85cf22
yujaemin <woawoa0513@gmail.com>
2022-10-21 13:43:32 +0900

    20221021

---
+++ BOJ/src/정렬/수정렬하기2_2751/Main.java
@@ -0,0 +1,28 @@
+package 정렬.수정렬하기2_2751;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int x : arr) {
+            sb.append(x).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
