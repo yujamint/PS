621b957dc1442ef9679a56f7cf275dfa8c5adf80
yujaemin <woawoa0513@gmail.com>
2022-10-22 19:53:45 +0900

    20221022

--- BOJ/src/스택/괄호_9012/Main.java
+++ BOJ/src/스택/괄호_9012/Main.java
@@ -1,4 +1,40 @@
 package 스택.괄호_9012;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class Main {
+
+    public static String isVPS(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            if (c == '(') stack.push(c);
+            else {
+                if (stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()) return "YES";
+        else return "NO";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+
+            String result = isVPS(input);
+            sb.append(result).append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
