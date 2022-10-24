5a7b4cd17ebc5854ebb2af80ebb973000d50bb63
yujaemin <woawoa0513@gmail.com>
2022-10-24 13:27:28 +0900

    20221024

---
+++ BOJ/src/문자열처리/단어길이재기_2473/Main.java
@@ -0,0 +1,15 @@
+package 문자열처리.단어길이재기_2473;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        System.out.println(input.length());
+    }
+}
