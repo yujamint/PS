289cc4b552a1a77a974cb762021c1586e83475f2
yujamint <71512749+yujamint@users.noreply.github.com>
2022-12-13 21:09:05 +0900

    [level 2] Title: 전화번호 목록, Time: 275.88 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,8 +4,8 @@ class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
 
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i].indexOf(phone_book[i - 1]) == 0) {
                 return false;
             }
         }
