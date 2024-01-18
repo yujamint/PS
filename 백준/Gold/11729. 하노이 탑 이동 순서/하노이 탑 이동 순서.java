9fece643bb3670ff46f4649f5d072c2a1dd94c67
Jae Min Yu <woawoa0513@gmail.com>
2024-01-18 15:40:27 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 548 ms, Memory: 46672 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int n;
+    static StringBuilder sb;
+
+    public static void DFS(int N, int start, int mid, int to) {
+        if (N == 1) {
+            sb.append(start).append(' ').append(to).append('\n');
+            return;
+        }
+
+        DFS(N - 1, start, to, mid);
+
+        sb.append(start).append(' ').append(to).append('\n');
+
+        DFS(N - 1, mid, start, to);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        sb.append((int) Math.pow(2, n) - 1).append('\n');
+        DFS(n, 1, 2, 3);
+
+        System.out.println(sb);
+    }
+}
