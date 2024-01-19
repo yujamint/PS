10bd735d6a8b197809158aac24dc27ffcd749c37
Jae Min Yu <woawoa0513@gmail.com>
2024-01-19 16:21:27 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static StringBuilder sb;
+    static int[] arr = new int[9];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        DFS(0, 1);
+        System.out.println(sb);
+    }
+
+    private static void DFS(int count, int at) {
+        if (count == m) {
+            for (int i = 0; i < m; i++) sb.append(arr[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+        for (int i = at; i <= n; i++) {
+            arr[count] = i;
+            DFS(count + 1, i + 1);
+        }
+    }
+}
