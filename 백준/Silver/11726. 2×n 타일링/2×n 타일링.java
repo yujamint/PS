6e2afb3568ea9f97febb252848a3ee4725dc0252
Jae Min Yu <woawoa0513@gmail.com>
2024-01-23 14:41:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        int[] dp = new int[1_001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10_007;
+        }
+        System.out.println(dp[n]);
+    }
+}
