3e90a20d5e19d64231099ec8c7b36cc2a5a384d6
Jae Min Yu <woawoa0513@gmail.com>
2024-01-23 16:07:55 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1384 ms, Memory: 80680 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][][] dp = new int[21][21][21];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String input;
+
+        for (int i = 0; i <= 20; i++) {
+            for (int j = 0; j <= 20; j++) {
+                for (int k = 0; k <= 20; k++) {
+                    if (i == 0 || j == 0 | k == 0) {
+                        dp[i][j][k] = 1;
+                        continue;
+                    }
+                    if (i < j && j < k) {
+                        dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k];
+                        continue;
+                    }
+                    dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k - 1];
+                }
+            }
+        }
+        while (!(input = br.readLine()).equals("-1 -1 -1")) {
+            StringTokenizer st = new StringTokenizer(input);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int result = getResult(a, b, c);
+            sb.append(String.format("w(%d, %d, %d) = ", a, b, c)).append(result).append("\n");
+
+        }
+        System.out.println(sb);
+    }
+
+    private static int getResult(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) return 1;
+        if (a > 20 || b > 20 || c > 20) return dp[20][20][20];
+        return dp[a][b][c];
+    }
+}
