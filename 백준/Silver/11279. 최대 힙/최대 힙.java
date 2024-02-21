e8a1edef3f326859653c1a9cba90a1285db19eb9
Jae Min Yu <woawoa0513@gmail.com>
2024-02-21 20:37:27 +0900

    [Silver II] Title: 최대 힙, Time: 328 ms, Memory: 26544 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                int result = queue.isEmpty() ? 0 : queue.poll();
+                sb.append(result).append("\n");
+            }
+            else {
+                queue.offer(num);
+            }
+        }
+        System.out.println(sb);
+    }
+}
