2fd10dec92857a5b7e9713bb7ca129807cddff71
Jae Min Yu <woawoa0513@gmail.com>
2024-02-26 13:01:32 +0900

    [Gold V] Title: 동전 1, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] coins = new int[n];
+        int[] dp = new int[k + 1];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(coins);
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j <= k; j++) {
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        System.out.println(dp[k]);
+    }
+}
