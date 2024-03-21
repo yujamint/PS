e64d3d5d7661b44b356e4926a3d89047ed60a329
Jae Min Yu <woawoa0513@gmail.com>
2024-03-21 15:19:10 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1088 ms, Memory: 89824 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, count = 1;
+    static int[] ch;
+    static List<List<Integer>> edges;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        ch = new int[n + 1];
+        edges = new ArrayList<>(n + 1);
+        for (int i = 0; i <= n; i++) {
+            edges.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            edges.get(v1).add(v2);
+            edges.get(v2).add(v1);
+        }
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(edges.get(i));
+        }
+
+        BFS(r);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(ch[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void BFS(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+        ch[v] = count++;
+        while (!queue.isEmpty()) {
+            int from = queue.poll();
+            List<Integer> edge = edges.get(from);
+            for (int to : edge) {
+                if (ch[to] == 0) {
+                    ch[to] = count++;
+                    queue.offer(to);
+                }
+            }
+        }
+    }
+
+}
