6efb88bd8efb97a79400ad84831c3ba6ed4369c2
Jae Min Yu <woawoa0513@gmail.com>
2024-05-04 10:49:15 +0900

    [Silver V] Title: 최소공배수, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        if (a == 0 || b == 0) {
+            System.out.println(0);
+            return;
+        }
+        int gcd = 1;
+        for (int i = 1; i <= Math.min(a, b); i++) {
+            if (a % i == 0 && b % i == 0) gcd = i;
+        }
+        System.out.println(a * b / gcd);
+    }
+
+}
