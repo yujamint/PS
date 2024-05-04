dec3e399a17a57d5f9b8dec4c29eec7d085fa772
Jae Min Yu <woawoa0513@gmail.com>
2024-05-04 11:00:06 +0900

    [Silver V] Title: 최소공배수, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.java
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -10,15 +10,16 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         long a = Long.parseLong(st.nextToken());
         long b = Long.parseLong(st.nextToken());
-        if (a == 0 || b == 0) {
-            System.out.println(0);
-            return;
-        }
-        int gcd = 1;
-        for (int i = 1; i <= Math.min(a, b); i++) {
-            if (a % i == 0 && b % i == 0) gcd = i;
-        }
+        long gcd = 1;
+        if (a > b) gcd = euclidean(a, b);
+        else gcd = euclidean(b, a);
         System.out.println(a * b / gcd);
+    }
+
+    private static long euclidean(long a, long b) {
+        long r = a % b;
+        if (r == 0) return b;
+        else return euclidean(b, r);
     }
 
 }
