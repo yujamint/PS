399816869756d103d02a27489a2c667f469dd80f
Jae Min Yu <woawoa0513@gmail.com>
2024-05-05 22:02:55 +0900

    [Silver V] Title: 창문 닫기, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for(int i = 1; i <= n; i = ++cnt * cnt) {
+            
+        }
+        System.out.println(cnt - 1);
+    }
+
+}
