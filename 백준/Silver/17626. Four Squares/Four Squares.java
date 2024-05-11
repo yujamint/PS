27160a3778582040c592067d3d368d1d473f4e4d
Jae Min Yu <woawoa0513@gmail.com>
2024-05-11 22:03:38 +0900

    [Silver III] Title: Four Squares, Time: 152 ms, Memory: 14500 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            for (int j = 2; j < i; j++) {
+                if (j * j > i) break;
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+
+}
