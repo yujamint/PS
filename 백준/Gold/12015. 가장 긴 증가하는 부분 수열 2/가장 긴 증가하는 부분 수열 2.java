8de11823d02526fd976bef4c6cae116edc0a7f19
Jae Min Yu <woawoa0513@gmail.com>
2024-05-18 16:23:15 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 640 ms, Memory: 121064 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -6,8 +6,11 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static int[] arr;
-    static List<Integer> LIS = new ArrayList<>();
+    static List<Integer> lis = new ArrayList<>();
+    static int pos = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -16,31 +19,26 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int max = 1;
-        LIS.add(arr[0]);
-        for (int i = 1; i < n; i++) {
-            if (arr[i] > LIS.get(LIS.size() - 1)) {
-                LIS.add(arr[i]);
-            }
-
-            else {
-                int idx = replaceIndex(arr[i]);
-                LIS.set(idx, arr[i]);
+        lis.add(0);
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > lis.get(pos)) {
+                lis.add(arr[i]);
+                pos++;
+            } else {
+                int idx = binarySearch(1, pos, i);
+                lis.set(idx, arr[i]);
             }
         }
-        System.out.println(LIS.size());
+        System.out.println(lis.size() - 1);
     }
 
-    private static int replaceIndex(int num) {
-        int lo = 0, hi = LIS.size();
-
-        while (lo < hi) {
-            int mid = (lo + hi) / 2;
-
-            if (LIS.get(mid) < num) lo = mid + 1;
-            else hi = mid;
+    private static int binarySearch(int s, int e, int cur) {
+        while (s < e) {
+            int mid = (s + e) / 2;
+            if (lis.get(mid) < arr[cur]) s = mid + 1;
+            else e = mid;
         }
-
-        return lo;
+        return s;
     }
+
 }
