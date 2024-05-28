1be325e189608ae110554ab2f7655da6922b37b6
Jae Min Yu <woawoa0513@gmail.com>
2024-05-28 13:39:23 +0900

    [Silver II] Title: N과 M (12), Time: 144 ms, Memory: 14552 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr, answer;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        answer = new int[m];
+        visited = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        DFS(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void DFS(int startIdx, int count) {
+        if (count == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(answer[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = startIdx; i < n; i++) {
+            if (i < n - 1 && arr[i] == arr[i + 1]) continue;
+            answer[count] = arr[i];
+            DFS(i,count + 1);
+        }
+    }
+
+}
