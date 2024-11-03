e185f4d077d194dd7793dee1dbaeea4baec3f97c
Jae Min Yu <woawoa0513@gmail.com>
2024-11-03 14:13:52 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 34.07 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int solution(int[] diffs, int[] times, long limit) {
+        int lo = 1, hi = getMaxDiff(diffs);
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            
+            long time = calculateTotalTime(diffs, times, mid);
+            if (time > limit) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+    
+    private int getMaxDiff(int[] diffs) {
+        int max = 0;
+        for(int i = 0; i < diffs.length; i++) {
+            max = Math.max(max, diffs[i]);
+        }
+        return max;
+    }
+    
+    private long calculateTotalTime(int[] diffs, int[] times, int level) {
+        long sum = 0L;
+        for (int i = 0; i < diffs.length; i++) {
+            int diff = diffs[i];
+            int time = times[i];
+            
+            if (diff <= level) sum += time;
+            else {
+                int prevTime = i == 0 ? 0 : times[i - 1];
+                int solvingTime = (time + prevTime) * (diff - level) + time;
+                sum += solvingTime;
+            }
+        }
+        return sum;
+    }
+}
