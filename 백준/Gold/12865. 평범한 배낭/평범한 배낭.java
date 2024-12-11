b668ce48fbf40bede7c806dc259569ec7b27e7a3
Jae Min Yu <woawoa0513@gmail.com>
2024-12-11 10:06:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 53752 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,36 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] wArr, vArr;
+
     static int[][] dp;
+    static int[] weight, value;
+    // dp(n개 물건, 사용 가능한 배낭 무게) = 가치 최대값
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        wArr = new int[n + 1];
-        vArr = new int[n + 1];
-        int k = Integer.parseInt(st.nextToken());
-        dp = new int[n + 1][k + 1];
-        int min = Integer.MAX_VALUE;
+        int maxWeight = Integer.parseInt(st.nextToken());
+        dp = new int[n + 1][maxWeight + 1];
+        weight = new int[n + 1];
+        value = new int[n + 1];
+
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            wArr[i] = Integer.parseInt(st.nextToken());
-            vArr[i] = Integer.parseInt(st.nextToken());
-            min = Math.min(min, wArr[i]);
+            weight[i] = Integer.parseInt(st.nextToken());
+            value[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 1; i <= n; i++) {
-            for (int j = min; j <= k; j++) {
-                // i번째 물건을 넣으면 무게를 넘길 때
-                if (wArr[i] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                }
-
-                else {
-                    dp[i][j] = Math.max(dp[i - 1][j - wArr[i]] + vArr[i], dp[i - 1][j]);
-                }
+            int w = weight[i];
+            for (int j = 0; j <= maxWeight; j++) {
+                if (j >= w) dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + value[i]);
+                else dp[i][j] = dp[i - 1][j];
             }
         }
-        System.out.println(dp[n][k]);
+
+        System.out.println(dp[n][maxWeight]);
     }
 }
