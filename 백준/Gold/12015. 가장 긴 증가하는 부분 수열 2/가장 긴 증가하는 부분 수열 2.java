148e9f248a14be169934cdf778f4adcbaa37f537
Jae Min Yu <woawoa0513@gmail.com>
2025-01-27 18:23:34 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 604 ms, Memory: 121064 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -9,36 +9,38 @@ public class Main {
 
     static int[] arr;
     static List<Integer> lis = new ArrayList<>();
-    static int pos = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
         lis.add(0);
+        int pos = 0;
         for (int i = 0; i < n; i++) {
             if (arr[i] > lis.get(pos)) {
                 lis.add(arr[i]);
                 pos++;
             } else {
-                int idx = binarySearch(1, pos, i);
+                int idx = binarySearch(pos, arr[i]);
                 lis.set(idx, arr[i]);
             }
         }
         System.out.println(lis.size() - 1);
     }
 
-    private static int binarySearch(int s, int e, int cur) {
-        while (s < e) {
-            int mid = (s + e) / 2;
-            if (lis.get(mid) < arr[cur]) s = mid + 1;
-            else e = mid;
+    private static int binarySearch(int end, int cur) {
+        int start = 1;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (lis.get(mid) < cur) start = mid + 1;
+            else end = mid;
         }
-        return s;
+        return start;
     }
-
 }
