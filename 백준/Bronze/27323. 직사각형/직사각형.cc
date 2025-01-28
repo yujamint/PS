a636c85af126627016d2fc80d42f441b76f2292f
Jae Min Yu <woawoa0513@gmail.com>
2025-01-28 22:05:56 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,19 @@
+//
+// Created by 유재민 on 2025. 1. 28..
+//
+
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> b;
+    cout << a * b << '\n';
+    return 0;
+}
