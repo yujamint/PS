a7961c345e5db7ddb2268ed53e2f2f601e888f24
Jae Min Yu <woawoa0513@gmail.com>
2025-02-05 23:26:38 +0900

    [Gold V] Title: 최소비용 구하기, Time: 512 ms, Memory: 52084 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,79 +2,84 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
+
+class Edge implements Comparable<Edge> {
+    int to;
+    long cost;
+
+    public Edge(int to, long cost) {
+        this.to = to;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return (int) (this.cost - o.cost);
+    }
+}
 
 public class Main {
 
-    private static final int INF = 987654321;
-
-    static class Edge implements Comparable<Edge> {
-        int to;
-        int weight;
-
-        public Edge(int to, int weight) {
-            this.to = to;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.weight - o.weight;
-        }
-    }
-
+    static final long INF = 100_000L * 100_000 + 1;
+    static int n, m;
     static List<List<Edge>> graph = new ArrayList<>();
-    static int[] dist;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        dist = new int[n + 1];
-        visited = new boolean[n + 1];
-        Arrays.fill(dist, INF);
-        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
+        visited = new boolean[n + 1];
+
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            graph.get(from).add(new Edge(to, weight));
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(from).add(new Edge(to, cost));
         }
-        st = new StringTokenizer(br.readLine());
-        int src = Integer.parseInt(st.nextToken());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int source = Integer.parseInt(st.nextToken());
         int dest = Integer.parseInt(st.nextToken());
-        dist[src] = 0;
-        dijkstra(src);
+
+        long[] dist = dijkstra(source);
         System.out.println(dist[dest]);
     }
 
-    private static void dijkstra(int start) {
-        Queue<Edge> queue = new PriorityQueue<>();
-        queue.offer(new Edge(start, 0));
-        while (!queue.isEmpty()) {
-            int cur;
-            do {
-                cur = queue.poll().to;
-            } while (!queue.isEmpty() && visited[cur]);
-            if (visited[cur]) break;
-            visited[cur] = true;
+    private static long[] dijkstra(int from) {
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        queue.offer(new Edge(from, 0));
 
-            for (Edge edge : graph.get(cur)) {
-                if (dist[edge.to] > dist[cur] + edge.weight) {
-                    dist[edge.to] = dist[cur] + edge.weight;
+        long[] dist = new long[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (i != from) dist[i] = INF;
+        }
+
+        while (!queue.isEmpty()) {
+            Edge cur;
+            do {
+                cur = queue.poll();
+            } while (!queue.isEmpty() && visited[cur.to]);
+            if (visited[cur.to]) break;
+
+            visited[cur.to] = true;
+
+            for (Edge edge : graph.get(cur.to)) {
+                if (dist[edge.to] > dist[cur.to] + edge.cost) {
+                    dist[edge.to] = dist[cur.to] + edge.cost;
                     queue.offer(new Edge(edge.to, dist[edge.to]));
                 }
             }
         }
-    }
 
+        return dist;
+    }
 }
