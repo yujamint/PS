2af83694eba308170a69e7d5b31445bf27a61a1f
Jae Min Yu <woawoa0513@gmail.com>
2025-02-27 23:09:15 +0900

    [level 3] Title: 등산코스 정하기, Time: 39.94 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/README.md
+++ 프로그래머스/3/118669. 등산코스 정하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등산코스 정하기 - 118669 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118669?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118669?language=cpp) 
 
 ### 성능 요약
 
-메모리: 67.8 MB, 시간: 432.55 ms
+메모리: 53.4 MB, 시간: 39.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 23:00:38
+2025년 02월 27일 23:09:13
 
 ### 문제 설명
 
--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 const int INF = 10000001;
-int len;
+int len, minSummit = 50001, minIntensity = INF;
 vector<vector<pair<int, int>>> graph;
 bool isSummit[50001], isGate[50001];
 
@@ -18,19 +18,30 @@ struct cmp {
     }
 };
 
-vector<int> dijkstra(int gate) {
+void dijkstra(vector<int> gates) {
     // pair: node, gate부터 node까지의 최소 intensity
     priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> pq;
-    pq.push(make_pair(gate, 0));
-    
     vector<int> intensity(len + 1, INF);
-    intensity[gate] = 0;
+    for (auto &gate : gates) {
+        pq.push(make_pair(gate, 0));
+        intensity[gate] = 0;
+    }
     
     while (!pq.empty()) {
         pair<int, int> t = pq.top();
         int cur = t.first, curIntensity = t.second;
         pq.pop();
-        if (isSummit[cur]) break;
+        
+        if (curIntensity > minIntensity) continue;
+        if (curIntensity > intensity[cur]) continue;
+        
+        if (isSummit[cur]) {
+            if (minIntensity > curIntensity || (minIntensity == curIntensity && minSummit > cur)) {
+                minIntensity = curIntensity;
+                minSummit = cur;
+            } 
+            continue;
+        }
         
         for (auto &edge : graph[cur]) {
             int next = edge.first, nextIntensity = edge.second;
@@ -42,7 +53,7 @@ vector<int> dijkstra(int gate) {
         }
     }
     
-    return intensity;
+    // return intensity;
 }
 
 vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
@@ -66,24 +77,10 @@ vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector
     }
     
     // 2. 출입구 순회
-    int minIntensity = INF;
-    int prevSummit = 50001;
+    // int minIntensity = INF;
+    // int prevSummit = 50001;
     vector<int> answer(2);
-    for (auto &gate : gates) {
-        vector<int> intensity = dijkstra(gate);
-        for (auto &summit : summits) {
-            if (minIntensity == intensity[summit] && prevSummit > summit) {
-                answer[0] = summit;
-                prevSummit = summit;
-            }
-            if (minIntensity > intensity[summit]) {
-                minIntensity = min(minIntensity, intensity[summit]);
-                answer[0] = summit;
-                answer[1] = minIntensity;
-                prevSummit = summit;
-            }
-        }
-    }
+    dijkstra(gates);
 
-    return answer;
+    return {minSummit, minIntensity};
 }
