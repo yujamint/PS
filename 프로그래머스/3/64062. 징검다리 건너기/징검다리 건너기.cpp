7260758ad1ef86cedf748bf9e639eb021f00fa62
Jae Min Yu <woawoa0513@gmail.com>
2025-03-01 00:19:44 +0900

    [level 3] Title: 징검다리 건너기, Time: 37.05 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <iostream>
+#include <memory.h>
 
 using namespace std;
 
@@ -12,17 +13,35 @@ struct cmp {
 };
 
 priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
-int n, cnt = 0;
+int n, cnt = 0, p[200000], sz[200000];
 bool used[200000];
 
+int find(int x) {
+    if (p[x] < 0) return x;
+    return p[x] = find(p[x]);
+}
+
+void unionn(int x, int y) {
+    x = find(x);
+    y = find(y);
+    
+    if (x == y) return;
+    
+    if (sz[x] < sz[y]) {
+        p[x] = y;
+        sz[y] += sz[x];
+    } else {
+        p[y] = x;
+        sz[x] += sz[y];
+    }
+}
+
 int solution(vector<int> stones, int k) {
+    memset(p, -1, sizeof(p));
     n = stones.size();
     for (int i = 0; i < n; i++) {
+        sz[i] = 1;
         pq.push(make_pair(i, stones[i]));
-    }
-    
-    if (n == 1) {
-        return stones[0];
     }
     
     while (!pq.empty()) {    
@@ -35,17 +54,15 @@ int solution(vector<int> stones, int k) {
             idx = cur.first, num = cur.second;
             usedTemp.push_back(idx);
         } while (!pq.empty() && pq.top().second == num);
-        //
         
         cnt = num;
         
         for (auto &usedIdx : usedTemp) {
             used[usedIdx] = true;
             int usedCnt = 1;
-            int left = usedIdx, right = usedIdx;
-            while (--left >= 0 && used[left]) usedCnt++;
-            while (++right < n && used[right]) usedCnt++;
-            if (usedCnt >= k) {
+            if (usedIdx - 1 >= 0 && used[usedIdx - 1]) unionn(usedIdx, usedIdx - 1);
+            if (usedIdx + 1 < n && used[usedIdx + 1]) unionn(usedIdx, usedIdx + 1);
+            if (sz[find(usedIdx)] >= k) {
                 return cnt;
             }
         }
